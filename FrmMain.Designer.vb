b3653f115c44919a4e37a904cd12375a4493c435
FSKMTM <kmtm@fs.uni-lj.si>
2017-03-13 12:46:21 +0100

    Initial commit

---
+++ FrmMain.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BtnRecord = New System.Windows.Forms.Button()
+        Me.BtnInit = New System.Windows.Forms.Button()
+        Me.BtnCalibrate = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.BtnStart.Location = New System.Drawing.Point(12, 115)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(60, 34)
+        Me.BtnStart.TabIndex = 0
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(260, 57)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "0.000"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        '
+        'BtnRecord
+        '
+        Me.BtnRecord.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.BtnRecord.Location = New System.Drawing.Point(78, 115)
+        Me.BtnRecord.Name = "BtnRecord"
+        Me.BtnRecord.Size = New System.Drawing.Size(60, 34)
+        Me.BtnRecord.TabIndex = 1
+        Me.BtnRecord.Text = "Record"
+        Me.BtnRecord.UseVisualStyleBackColor = True
+        '
+        'BtnInit
+        '
+        Me.BtnInit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnInit.Location = New System.Drawing.Point(212, 115)
+        Me.BtnInit.Name = "BtnInit"
+        Me.BtnInit.Size = New System.Drawing.Size(60, 34)
+        Me.BtnInit.TabIndex = 3
+        Me.BtnInit.Text = "Init"
+        Me.BtnInit.UseVisualStyleBackColor = True
+        '
+        'BtnCalibrate
+        '
+        Me.BtnCalibrate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnCalibrate.Location = New System.Drawing.Point(146, 115)
+        Me.BtnCalibrate.Name = "BtnCalibrate"
+        Me.BtnCalibrate.Size = New System.Drawing.Size(60, 34)
+        Me.BtnCalibrate.TabIndex = 2
+        Me.BtnCalibrate.Text = "Calibrate"
+        Me.BtnCalibrate.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(8, 79)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 0
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(271, 20)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 161)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.BtnCalibrate)
+        Me.Controls.Add(Me.BtnInit)
+        Me.Controls.Add(Me.BtnRecord)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnStart)
+        Me.MinimumSize = New System.Drawing.Size(300, 200)
+        Me.Name = "FrmMain"
+        Me.Text = "Friction Meter"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnStart As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents BtnRecord As System.Windows.Forms.Button
+    Friend WithEvents BtnInit As System.Windows.Forms.Button
+    Friend WithEvents BtnCalibrate As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ FrmMain.vb
@@ -0,0 +1,188 @@
+﻿Public Class FrmMain
+    Declare Auto Function SendMessage Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal msg As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    Private clsFF3x_API As New FF3x_API.FF3x_API
+    Dim bytResponse() As Byte = Nothing
+    Dim outfile As System.IO.StreamWriter
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Label1.Font = New Font("Arial", Label1.Height * 0.6)
+        InitFF()
+    End Sub
+    Private Function ConvertPinName2PinBlock(ByVal strPin As String) As Byte
+        Dim bytPin() As Byte = System.Text.Encoding.ASCII.GetBytes(strPin)
+        Return CByte(bytPin(0) - &H37)
+    End Function
+    Private Function ConvertPinName2PinNumber(ByVal strPin As String) As Byte
+        Dim bytPin() As Byte = System.Text.Encoding.ASCII.GetBytes(strPin)
+        If bytPin.Length = 3 Then   ' two-digits pin number
+            Return CByte((bytPin(1) - 39) + (bytPin(2) - 48))
+        End If
+        Return CByte(bytPin(1) - 48)
+    End Function
+
+    Private Function HexToByte(ByVal strIn As String) As Byte
+        Return CByte(CInt("&H" & strIn.Substring(2, 2)))
+    End Function
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        If Timer1.Enabled = True Then
+            Timer1.Stop()
+            BtnStart.Text = "Start"
+        Else
+            Timer1.Start()
+            BtnStart.Text = "Stop"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim friction As Double
+        friction = (readAnalog("B1") - My.Settings.zeroOffset) / (My.Settings.ownWeight - My.Settings.zeroOffset)
+        Label1.Text = friction.ToString("F3")
+    End Sub
+
+    Private Sub BtnRecord_Click(sender As Object, e As EventArgs) Handles BtnRecord.Click
+        Dim result As DialogResult
+        SaveFileDialog1.ValidateNames = True
+        SaveFileDialog1.DefaultExt = ".txt"
+        SaveFileDialog1.Filter = "Text files|*.txt|All files|*.*"
+        SaveFileDialog1.OverwritePrompt = True
+        result = SaveFileDialog1.ShowDialog()
+        If result = Windows.Forms.DialogResult.Cancel Then
+            Exit Sub
+        End If
+        recordFile(SaveFileDialog1.FileName)
+    End Sub
+
+    Private Sub recordFile(filename As String)
+        Dim friction As Double
+        Dim readingCount As Integer
+        Dim startTime As Date
+        Dim timeDiff As TimeSpan
+        Dim millis As Long = 0
+        Timer1.Stop()
+        BtnStart.Text = "Start"
+        BtnStart.Enabled = False
+        BtnRecord.Enabled = False
+        BtnCalibrate.Enabled = False
+        BtnInit.Enabled = False
+        MsgBox("Press OK, then drag the device for 5 seconds.", MsgBoxStyle.Information, "Friction Meter")
+        outfile = My.Computer.FileSystem.OpenTextFileWriter(filename, False)
+        outfile.WriteLine("Time [ms]" & vbTab & "Ft/Fn")
+        SendMessage(ProgressBar1.Handle, (1024 + 16), 3, 0)
+        For readingCount = 0 To 199
+            timeDiff = New TimeSpan(0)
+            friction = (readAnalog("B1") - My.Settings.zeroOffset) / (My.Settings.ownWeight - My.Settings.zeroOffset)
+            Label1.Text = friction.ToString("F3")
+            ProgressBar1.Value = 0.5 * (readingCount + 1)
+            Me.Refresh()
+            startTime = DateTime.Now
+            While timeDiff.TotalMilliseconds < 25
+                timeDiff = DateTime.Now.Subtract(startTime)
+                'Label1.Text = timeDiff.TotalMilliseconds.ToString
+                'Label1.Refresh()
+                'My.Application.DoEvents()
+            End While
+            millis += timeDiff.TotalMilliseconds
+            outfile.WriteLine(millis.ToString & vbTab & friction.ToString("F3"))
+        Next
+        outfile.Close()
+        SendMessage(ProgressBar1.Handle, (1024 + 16), 1, 0)
+        ProgressBar1.Value = 100
+        BtnStart.Enabled = True
+        BtnRecord.Enabled = True
+        BtnCalibrate.Enabled = True
+        BtnInit.Enabled = True
+    End Sub
+    Private Function readAnalog(pin As String) As Double
+        If clsFF3x_API.GetAnalogInput(ConvertPinName2PinBlock("B"), ConvertPinName2PinNumber(pin), bytResponse) = False Then
+            Return Double.NaN
+        Else
+            Dim dblValue As Double = (((bytResponse(2) * 256) + bytResponse(3)) / 1023)
+            If bytResponse(1) = CByte(&H33) Then
+                Return dblValue * 3.3
+            Else
+                Return dblValue * 5
+            End If
+        End If
+    End Function
+    Private Sub InitFF()
+        Dim intNumOfDevices As Integer
+        clsFF3x_API.CloseComm()
+        intNumOfDevices = clsFF3x_API.GetNumOfDevices
+        ' check if any FF3x device is present
+        If intNumOfDevices = 0 Then
+            MsgBox("No FF3x device found.", MsgBoxStyle.Critical, "Friction Meter")
+            'Me.Close()
+            Return
+        End If
+        ' get 1st device path
+        If clsFF3x_API.GetPath(1) = False Then
+            MsgBox("Unable to locate FF3x device.", MsgBoxStyle.Critical, "Friction Meter")
+            'Me.Close()
+            Return
+        End If
+        ' connect to the device
+        If clsFF3x_API.OpenComm = False Then
+            MsgBox("Unable to open FF3x device.", MsgBoxStyle.Critical, "Friction Meter")
+            'Me.Close()
+            Return
+        End If
+    End Sub
+    Private Sub Calibrate()
+        Dim result As MsgBoxResult
+        Dim reading As Double
+        Dim readingCount As Integer
+        Dim readingSum As Double = 0
+        Dim zeroOffset As Double = 0
+        Dim ownWeight As Double = 0.6
+        result = MsgBox("Current zero offset is " & My.Settings.zeroOffset.ToString("F3") & ", current device weight is " & My.Settings.ownWeight.ToString("F3") & vbCrLf & "Leave the device with zero force on the string and press OK.", MsgBoxStyle.Information Or MsgBoxStyle.OkCancel, "Friction Meter")
+        If result = MsgBoxResult.Cancel Then
+            Exit Sub
+        End If
+        For readingCount = 0 To 49
+            reading = readAnalog("B1")
+            readingSum += reading
+            Label1.Text = reading.ToString("F3")
+            ProgressBar1.Value = (readingCount + 1) * 2
+            Me.Refresh()
+            System.Threading.Thread.Sleep(50)
+        Next
+        zeroOffset = readingSum / 50
+        result = MsgBox("Zero offset is " & zeroOffset.ToString & ". Now suspend the device by the string and press OK.", MsgBoxStyle.Information Or MsgBoxStyle.OkCancel, "Friction Meter")
+        If result = MsgBoxResult.Cancel Then
+            Exit Sub
+        End If
+        readingSum = 0
+        For readingCount = 0 To 49
+            reading = readAnalog("B1")
+            readingSum += reading
+            Label1.Text = reading.ToString("F3")
+            ProgressBar1.Value = (readingCount + 1) * 2
+            Me.Refresh()
+            System.Threading.Thread.Sleep(50)
+        Next
+        ownWeight = readingSum / 50
+        result = MsgBox("Own weight of the device is " & ownWeight.ToString & ". Press OK to save the calibration results or cancel to keep the previous versions.", MsgBoxStyle.Information Or MsgBoxStyle.OkCancel, "Friction Meter")
+        If result = MsgBoxResult.Cancel Then
+            Exit Sub
+        End If
+        My.Settings.zeroOffset = zeroOffset
+        My.Settings.ownWeight = ownWeight
+    End Sub
+    Private Sub BtnInit_Click(sender As Object, e As EventArgs) Handles BtnInit.Click
+        Timer1.Stop()
+        BtnStart.Text = "Start"
+        InitFF()
+    End Sub
+
+    Private Sub BtnCalibrate_Click(sender As Object, e As EventArgs) Handles BtnCalibrate.Click
+        Timer1.Stop()
+        BtnStart.Text = "Start"
+        Calibrate()
+    End Sub
+
+    Private Sub FrmMain_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        Dim fhgt As Integer
+        fhgt = Math.Min(Label1.Height * 0.6, Label1.Width * 0.2)
+        Label1.Font = New Font("Arial", fhgt)
+    End Sub
+End Class
